929737ae03fb6189414c41ee6f29fcc3b673c587
Ritesh Narendra Chaudhari <76548605+Ritesh2408@users.noreply.github.com>
2022-03-13 10:07:01 +0530

    ..

--- welcomePage/app.js
+++ welcomePage/app.js
@@ -11,10 +11,3 @@ $(function () {
     });
 
 });
-
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("build"));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "build", "index.html"));
-    });
-}
